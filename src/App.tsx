849716620b12cbf7333c16f419851abba411288a
AlexMNet <emtamaldonado@gmail.com>
2023-03-07 21:37:40 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="h-screen overflow-x-hidden bg-black">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import headerLogo from '../assets/logo.svg';
+
+const Header = () => {
+  return (
+    <header className="bg-neutral-800 py-6 w-screen">
+      <img className="w-24 mx-auto" src={headerLogo} alt="Tesla Logo" />
+    </header>
+  );
+};
+
+export default Header;
